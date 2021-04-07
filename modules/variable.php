674a35ba0e31ba995d5e6fe91f6d3c763090f783
jakdev047 <jakfreelancer047@gmail.com>
2021-04-07 11:08:54 +0600

    various data type

---
+++ modules/variable.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+    variable of PHP
+
+    string -> 'Jubayer'
+    Integer -> 47
+    Float -> 47.047
+    Boolean -> true/false
+    Array -> array('red', 'green', 'blue')
+    null
+    Resources
+    object ->
+
+    declare keyword of php
+    
+    $name = 'jubayer'
+    define('PI',3,1416)
+
+*/
+
+echo("Variable of PHP!"."<br/>");
+
+$name = 'Jubayer';
+define('PI',3.1416);
+
+echo("Name: ".$name."<br/>");
+echo constant("PI");
